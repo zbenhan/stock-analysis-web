52f062aad7237ceaf9272aaba2617402a011827d
zbenhan <61318619+zbenhan@users.noreply.github.com>
2025-10-05 18:57:02 +0800

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,7 +6,7 @@ from stock_analysis.views import download_template
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('api/generate-chart/', views.generate_chart_api, name='generate_chart'),
+    path('api/get-stock-data/', views.get_stock_data_api, name='get_stock_data'),
     path('api/get-random-stocks/', views.get_random_stocks_api, name='get_random_stocks'),
     path('api/download-template/', download_template, name='download_template'),
 ]
