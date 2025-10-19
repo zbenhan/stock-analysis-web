22f61f392569ecc64dd0cf45bc26eb222433b57e
zbenhan <zbenhan@126.com>
2025-10-19 17:05:05 +0800

    Update articles-data.js

--- frontend/static/js/articles-data.js
+++ frontend/static/js/articles-data.js
@@ -20,7 +20,7 @@ const articlesData = {
         },
         {
             "id": 4,
-            "title": "法国密西西比泡沫：全国疯狂的“货币炼金术”",
+            "title": "法国密西西比泡沫：全国疯狂的货币炼金术",
             "url": "/articles/mississippi-bubble.html",
             "date": "2025-10-19",
             "category": "knowledge-research",
@@ -38,7 +38,7 @@ const articlesData = {
         },
         {
             "id": 6,
-            "title": "大萧条时期的“自杀浪潮”与“窗口擦鞋童”",
+            "title": "大萧条时期的自杀浪潮与窗口擦鞋童",
             "url": "/articles/great-depression-suicide-shoeshine.html",
             "date": "2025-10-19",
             "category": "knowledge-research",
